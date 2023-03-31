5f008bac1ff4ea9c523683b0dc0094f4c217c9fe
meiergt <meiergt@rose-hulman.edu>
2023-03-31 10:19:46 -0400

    3/31/23 FRiday, added methods to country and put different code in song

--- Eastern_European_Group/src/Country.java
+++ Eastern_European_Group/src/Country.java
@@ -1,7 +1,14 @@
-
+import java.util.ArrayList;
 
 public class Country {
-	//private ArrayList<Song>();
+	private ArrayList<Song> songs = new ArrayList<Song>();
 	private String name;
+	
+	
+	public void addSong(String name) {
+		this.songs.add(new Song(name));
+	}
+	
+	
 	
 }
--- Eastern_European_Group/src/Song.java
+++ Eastern_European_Group/src/Song.java
@@ -1,26 +1,48 @@
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.File;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.TargetDataLine;
 import javax.swing.JOptionPane;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javazoom.jl.player.Player;
+
+
 
 public class Song {
 	private String title;
-	private String artist;
-	private String album;
-	private Double length;
-	private Integer releaseYear;
+	private String artist = "Uknown";
+	private String album= "Uknown";
+	private Double length = 0.0;
+	private Integer releaseYear = 0;
+	private String filePath;
+	private Clip clip;
+	private String status;
 	
 	
 	
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public Song(String name) {
+		//this.filePath = ;
+		this.title = name;
+		
 		
 	}
 
+	public Song(String name, String artist, String album) {
+		//this.filePath = ;
+		this.title = name;
+		this.album = album;
+		this.artist = artist;
+			
+	}
+	
+	
 	public String getTitle() {
 		return this.title;
 	}
@@ -41,17 +63,60 @@ public class Song {
 		return this.releaseYear;
 	}
 	
-	public static void playSong() {
-		
-		InputStream music;
-		try {
-			music = new FileInputStream(new File(filepath));
-			AudioStream audios = new AudioInputStream((TargetDataLine) music);
-			AudioPlayer.player.start(audios);
-		}
-		catch(Exception e) {
-			JOptionPane.showMessageDialog(null, "error!");
+	
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public void setArtist(String person) {
+		this.artist = person;
+	}
+	
+	public void setAlbum(String disc) {
+		this.album = disc;
+	}
+	
+	public void setLength(Double length) {
+		this.length = length;
+	}
+	
+	public void setRelease(Integer year) {
+		this.releaseYear = year;
+	}
+	
+	
+	
+	public void playSong() {		
+		if(this.status != "playing") {
+			this.clip.start();
+			this.status = "playing";
 		}
 	}
+	
+	public void pauseSong() {		
+		if(this.status != "paused") {
+			this.clip.stop();
+			this.status = "paused";
+		}
+	}
+	
+	  public void SimpleAudioPlayer() // make this the constructor for the class
+		        throws UnsupportedAudioFileException,
+		        IOException, LineUnavailableException 
+		    {
+		        // create AudioInputStream object
+		        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(this.filePath).getAbsoluteFile());
+		          
+		        // create clip reference
+		        this.clip = AudioSystem.getClip();
+		          
+		        // open audioInputStream to the clip
+		        this.clip.open(audioInputStream);
+		          
+		        this.clip.loop(Clip.LOOP_CONTINUOUSLY);
+		        
+		        
+		    }
 	
 }
